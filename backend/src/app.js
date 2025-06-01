682f0338621aeb3ac6883221e3471815b57be0db
Joy Kiragu <sheerohkiragu@gmail.com>
2025-06-01 20:40:23 +0300

    frontend updates

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,40 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+// socket ui setup
+io.on('connection', (socket) => {
+  console.log('A user connected');
 
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+
+// Middleware
+app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 
+// Routes
 app.use('/api/v1', routes);
-
 app.use(errorHandler);
 
 module.exports = app;
